44955a62514cc7f2d70ca67f31f034f41454f1bd
Hello-LSY <leesy9520@naver.com>
2025-06-30 09:28:29 +0900

    refactor: 프로젝트명 mmd -> anonymous_diary 로 변경

--- src/main/java/com/anonymous_diary/ad_backend/controller/diary/crud/DiaryController.java
+++ src/main/java/com/anonymous_diary/ad_backend/controller/diary/crud/DiaryController.java
@@ -1,7 +1,6 @@
 package com.anonymous_diary.ad_backend.controller.diary.crud;
 
 import com.anonymous_diary.ad_backend.controller.diary.dto.*;
-import com.mumyungdiary.mmd_backend.controller.diary.dto.*;
 import com.anonymous_diary.ad_backend.domain.diary.Diary;
 import com.anonymous_diary.ad_backend.security.auth.UserPrincipal;
 import com.anonymous_diary.ad_backend.service.diary.DiaryService;
