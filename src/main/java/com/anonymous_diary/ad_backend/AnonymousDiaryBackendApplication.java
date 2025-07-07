5683bb067d6bab43733d9e5785e1fa48e8878efa
Hello-LSY <leesy9520@naver.com>
2025-07-07 13:51:06 +0900

    feature: 스케줄링 적용, yml 적용

--- src/main/java/com/anonymous_diary/ad_backend/AnonymousDiaryBackendApplication.java
+++ src/main/java/com/anonymous_diary/ad_backend/AnonymousDiaryBackendApplication.java
@@ -3,9 +3,11 @@ package com.anonymous_diary.ad_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class AnonymousDiaryBackendApplication {
 
 	public static void main(String[] args) {
