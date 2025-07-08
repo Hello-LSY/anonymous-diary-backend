b12c383814099f8b71bf8088ce9b40f32878f74e
Hello-LSY <leesy9520@naver.com>
2025-07-08 11:18:23 +0900

    refactor: 상수클래스 제거

--- src/main/java/com/anonymous_diary/ad_backend/config/SecurityConfig.java
+++ src/main/java/com/anonymous_diary/ad_backend/config/SecurityConfig.java
@@ -6,22 +6,18 @@ import com.anonymous_diary.ad_backend.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-import static com.anonymous_diary.ad_backend.domain.common.constants.URLConstants.LOCAL_URL;
-import static com.anonymous_diary.ad_backend.domain.common.constants.URLConstants.SERVER_URL;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
--- src/main/java/com/anonymous_diary/ad_backend/domain/common/constants/URLConstants.java
+++
@@ -1,7 +0,0 @@
-package com.anonymous_diary.ad_backend.domain.common.constants;
-
-public class URLConstants {
-    public static final String SERVER_URL = "https://anonymousdiary.vercel.app";
-    public static final String LOCAL_URL = "http://localhost:3000";
-
-}
